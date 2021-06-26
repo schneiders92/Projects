55d3ab474b6a799c62e66f464b15bf67bcbaa062
Schneider Sara <schneiders92@users.noreply.github.com>
2021-06-26 22:02:12 +0200

    writing the List component

--- birthday-reminder/src/List.js
+++ birthday-reminder/src/List.js
@@ -4,7 +4,17 @@
 function List(props){
   return (
     <>
-      <h2>list component</h2>
+        {props.data.map(person =>{
+            return(
+                <article key={person.id} className='person'>
+                    <img src={person.image} alt=""/>
+                    <div>
+                      <h4>{person.name}</h4>
+                      <p>{person.age} years</p>
+                    </div>
+                </article>
+            )
+        })}
     </>
   )
 }
